7c3e475e95a6db1231aee9a3e9150856bdde3400
Rudresh Raj <120813698+RudreshRaj001@users.noreply.github.com>
2024-07-26 14:48:30 +0530

    Time: 1 ms (98.99%), Space: 42.7 MB (54%) - LeetHub

--- 1421-find-numbers-with-even-number-of-digits/1421-find-numbers-with-even-number-of-digits.java
+++ 1421-find-numbers-with-even-number-of-digits/1421-find-numbers-with-even-number-of-digits.java
@@ -5,5 +5,13 @@ class Solution {
             count -= (int)(Math.log10(num) + 1) % 2;
         }
         return count;
+        // Math.log10(num) -> number of 10 power which equals nums
+        // eg: num = 100 --> 10^2 == 100
+        // eg: num = 1246 --> 10^3.09 == 1246
+        // doing (int)(Math.log10(num) + 1) will give no of digits
+        // (int)(Math.log10(num) + 1) % 2 --> gives 0 when even and 1 when odd
+
+        // means we subtract 1 everytime a odd no. appears form the count which contains length of the nums array times numbers
+        // at the we'll be left with the no. of even no. digits numbers
 	}
 }
