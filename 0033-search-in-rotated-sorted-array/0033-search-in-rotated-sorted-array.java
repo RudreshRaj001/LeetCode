9fb36854fd397c107f6408ce6e9a4113e62ce212
Rudresh Raj <120813698+RudreshRaj001@users.noreply.github.com>
2024-07-30 09:54:49 +0530

    Time: 0 ms (100%), Space: 42.4 MB (10.08%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -27,7 +27,7 @@ class Solution {
       if(mid > start && arr[mid] < arr[mid - 1]){
         return mid - 1;
       } 
-      if (arr[mid] <= arr[start]){
+      if (arr[mid] < arr[start]){
         end = mid - 1;
       } else {
         start = mid + 1;
