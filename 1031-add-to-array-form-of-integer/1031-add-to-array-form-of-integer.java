46bcd45c813b03554efb62f621d0f74978bc8a07
Rudresh Raj <120813698+RudreshRaj001@users.noreply.github.com>
2024-08-01 15:19:00 +0530

    Time: 2 ms (98.77%), Space: 46.3 MB (7.77%) - LeetHub

--- 1031-add-to-array-form-of-integer/1031-add-to-array-form-of-integer.java
+++ 1031-add-to-array-form-of-integer/1031-add-to-array-form-of-integer.java
@@ -1,7 +1,9 @@
 class Solution {
     public List<Integer> addToArrayForm(int[] num, int k) {
-        List<Integer> list = new ArrayList<Integer>(); 
-    //    LinkedList<Integer> list = new LinkedList(); 
+       // List<Integer> list = new ArrayList<Integer>(); 
+       // Internally uses a dynamic array. so inserting the element makes complexity worst
+       LinkedList<Integer> list = new LinkedList(); 
+       // Internally uses a doubly-linked list. hence easy to insert at the start
         int length = num.length;
         for(int i = length - 1; i >= 0; i--){
             list.add(0, (num[i]+k)%10);
