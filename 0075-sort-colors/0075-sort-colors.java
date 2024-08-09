cfe1b5b0e2466d1d9eb5bf9412c23dba3421dbb3
Rudresh Raj <120813698+RudreshRaj001@users.noreply.github.com>
2024-08-09 10:02:25 +0530

    Time: 0 ms (100%), Space: 41.7 MB (60.84%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -4,26 +4,26 @@ class Solution {
         int white = 0;
         int blue = 0;
         for(int i = 0; i < nums.length; i++){
-            if(nums[i] == 0){
+            if(nums[i]==0){
                 red++;
-            }else if(nums[i] == 1){
+            }else if(nums[i]==1){
                 white++;
             }else{
                 blue++;
             }
         }
         int j = 0;
-        while(red != 0){
+        while(red!=0){
             nums[j] = 0;
             j++;
             red--;
         }
-        while(white != 0){
+        while(white!=0){
             nums[j] = 1;
             j++;
             white--;
         }
-        while(blue != 0){
+        while(blue!=0){
             nums[j] = 2;
             j++;
             blue--;
