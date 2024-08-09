6580ac7846fb37ab0d44f198f54de4747e6a11d0
Rudresh Raj <120813698+RudreshRaj001@users.noreply.github.com>
2024-08-09 10:20:06 +0530

    Time: 0 ms (100%), Space: 57.3 MB (60.2%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -3,9 +3,9 @@ class Solution {
         int n = nums.length;
         k = k % n; //In case k is greater than n
 
-        reverse(nums, 0, n - 1);
-        reverse(nums, 0, k - 1);
-        reverse(nums, k, n - 1);
+        reverse(nums, 0, n - 1); // reverse entire array
+        reverse(nums, 0, k - 1); // reverse firest k elements
+        reverse(nums, k, n - 1); // reverse remaining elements
         
     }
     private void reverse(int[] nums,int start, int end){
