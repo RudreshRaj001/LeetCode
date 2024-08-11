e039ed0c634a36d7242e8f1601dc8cda126ad54d
Rudresh Raj <120813698+RudreshRaj001@users.noreply.github.com>
2024-08-11 17:37:11 +0530

    Time: 0 ms (100%), Space: 42.3 MB (10.98%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,56 +1,56 @@
 class Solution {
     public int search(int[] arr, int target) {
         int pivot = findPivot(arr);
-    // if you did not find a pivot, it means the array is not rotated
-    if(pivot == - 1){
-      return binarySearch(arr, target, 0, arr.length - 1);
-    }
+        // if you did not find a pivot, it means the array is not rotated
+        if (pivot == -1) {
+            return binarySearch(arr, target, 0, arr.length - 1);
+        }
 
-    // if pivot is found, you have found 2 ascending sorted arrays
-    if(arr[pivot] == target){
-      return pivot;
-    } else if(target >= arr[0]){
-      return binarySearch(arr, target, 0, pivot - 1);
-    } else {
-      return binarySearch(arr, target, pivot + 1, arr.length - 1);
-    }
-    }
-
-    public int findPivot(int[] arr){
-    int start = 0;
-    int end = arr.length - 1;
-    while (start <= end) {
-      int mid = start + (end - start)/2;
-      if (mid < end && arr[mid] > arr[mid + 1]) {
-        // mid < end because mid + 1 might give out of bound
-        return mid;
-      }
-      if(mid > start && arr[mid] < arr[mid - 1]){
-        // mid > start because mid - 1 might give out of bound
-        return mid - 1;
-      } 
-      if (arr[mid] <= arr[start]){ 
-        // As we have distinct values we need not worry about = case
-        end = mid - 1;
-      } else {
-        start = mid + 1;
-      }
-    }
-    return -1;
-  }
-
-  public int binarySearch(int[] arr, int target, int start, int end){
-    while(start<=end){
-        int mid = start + ((end - start) / 2);
-        if(target < arr[mid]){
-            end = mid - 1;
-        } else if (target > arr[mid]) {
-            start = mid  + 1;
+        // if pivot is found, you have found 2 ascending sorted arrays
+        if (arr[pivot] == target) {
+            return pivot;
+        } else if (target >= arr[0]) {
+            return binarySearch(arr, target, 0, pivot - 1);
         } else {
-            return mid;
+            return binarySearch(arr, target, pivot + 1, arr.length - 1);
         }
     }
 
-    return -1;
-}
+    public int findPivot(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (mid < end && arr[mid] > arr[mid + 1]) {
+                // mid < end because mid + 1 might give out of bound
+                return mid;
+            }
+            if (mid > start && arr[mid] < arr[mid - 1]) {
+                // mid > start because mid - 1 might give out of bound
+                return mid - 1;
+            }
+            if (arr[mid] <= arr[start]) {
+                // As we have distinct values we need not worry about = case
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    public int binarySearch(int[] arr, int target, int start, int end) {
+        while (start <= end) {
+            int mid = start + ((end - start) / 2);
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (target > arr[mid]) {
+                start = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+
+        return -1;
+    }
 }
