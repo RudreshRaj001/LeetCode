7c84d0ae3e74885712702daf9d9876cafd3ee2f8
Rudresh Raj <120813698+RudreshRaj001@users.noreply.github.com>
2024-08-13 21:00:41 +0530

    Time: 0 ms (100%), Space: 50.4 MB (32%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,13 +1,5 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        // 1 1 2 2 3 3 4 4 5 5 
-        // 0 1 2 3 4 5 6 7 8 9 
-
-        // 1 1 2 2 3 3 4 4 5  6 6 7 7
-        // 0 1 2 3 4 5       
-
-        // end = 13 -> odd
-        // 8
         int len = nums.length;
         int start = 0;
         int end = len - 1;
@@ -17,11 +9,13 @@ class Solution {
             // Ensure mid is even
             if(mid % 2 == 1){
                 mid--;
-            }
+            } 
 
             // Compare mid with its pair
             if (nums[mid] == nums[mid + 1]) {
                 // If equal, single element is after mid
+                // because there are exactly even elements before mid 
+                // and there is no chance that exactly one of them doesnt have a pair
                 start = mid + 2;
             } else {
                 // If not equal, single element is before or at mid
