1526410911db23c0c83395339fcd75a232643365
Rudresh Raj <120813698+RudreshRaj001@users.noreply.github.com>
2024-08-17 20:05:58 +0530

    Time: 5 ms (57.13%), Space: 41 MB (27.22%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.java
+++ 0441-arranging-coins/0441-arranging-coins.java
@@ -1,15 +1,15 @@
 class Solution {
     public int arrangeCoins(int n) {
-        return (int)(Math.sqrt(1 + 8 * (long)n ) - 1) / 2;
-        // Concept used is triangulation of numbers: https://www.youtube.com/watch?v=X3_h4w3jVsk
-        // https://chatgpt.com/share/4e6b9642-25e7-4c73-af81-1f9ddccd267e
-        // also known as the sum of first n natural numbers
+        // return (int)(Math.sqrt(1 + 8 * (long)n ) - 1) / 2;
+        // // Concept used is triangulation of numbers: https://www.youtube.com/watch?v=X3_h4w3jVsk
+        // // https://chatgpt.com/share/4e6b9642-25e7-4c73-af81-1f9ddccd267e
+        // // also known as the sum of first n natural numbers
 
-        // int i = 1;
-        // while(n > 0){
-        //     i++;
-        //     n -= i;
-        // }
-        // return i - 1;
+        int i = 1;
+        while(n > 0){
+            i++;
+            n -= i;
+        }
+        return i - 1;
     }
 }
