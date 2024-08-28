51e81a16a35cb4a4c38f9cc6d3380c2ec6a4d7a9
Rudresh Raj <120813698+RudreshRaj001@users.noreply.github.com>
2024-08-28 21:12:35 +0530

    Time: 0 ms (100%), Space: 45.3 MB (44.27%) - LeetHub

--- 0566-reshape-the-matrix/0566-reshape-the-matrix.java
+++ 0566-reshape-the-matrix/0566-reshape-the-matrix.java
@@ -3,26 +3,26 @@ class Solution {
         int l = mat.length;
         int b = mat[0].length;
 
-        if((l * b) != (r * c)) return mat;
+        if ((l * b) != (r * c))
+            return mat;
 
         int[][] ans = new int[r][c];
         int output_rows = 0;
         int output_cols = 0;
 
-        int i = 0;
-        while(i < l){
-            int j = 0;
-            while(j < b){
+        for (int i = 0; i < l; i++) {
+            for (int j = 0; j < b; j++) {
                 ans[output_rows][output_cols] = mat[i][j];
                 output_cols++;
-                if(output_cols == c){
+
+                // if the cols value reached then change the row and set the cols value to 0.
+                if (output_cols == c) {
                     output_cols = 0;
                     output_rows++;
                 }
-                j++;
             }
-            i++;
         }
+
         return ans;
     }
 }
