f312b9744f110d3a4af4edab138f890adeed5856
Rudresh Raj <120813698+RudreshRaj001@users.noreply.github.com>
2024-08-29 21:05:30 +0530

    Time: 2 ms (41.82%), Space: 40.3 MB (38.1%) - LeetHub

--- 0755-reach-a-number/0755-reach-a-number.java
+++ 0755-reach-a-number/0755-reach-a-number.java
@@ -1,7 +1,6 @@
 class Solution {
     public int reachNumber(int target) {
-        // if(target < 0) target *= -1;
-        target = Math.abs(target);
+        if(target < 0) target *= -1; // OR  target = Math.abs(target);
         int n = 1;
         int sum = 0;
 
@@ -9,14 +8,11 @@ class Solution {
             sum += n;
             n++;
         }
-        // if((sum - target) % 2 == 0){
-        // return n - 1;
-        // }else{
+        
         while ((sum - target) % 2 != 0) {
             sum += n;
             n++;
         }
         return n - 1;
-        // }
     }
 }
