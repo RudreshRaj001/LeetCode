c9350e15314d43e1e2a50d7ce29cecfcdb64f1d6
Rudresh Raj <120813698+RudreshRaj001@users.noreply.github.com>
2024-08-29 22:18:54 +0530

    Time: 0 ms (100%), Space: 40.2 MB (53%) - LeetHub

--- 0755-reach-a-number/0755-reach-a-number.java
+++ 0755-reach-a-number/0755-reach-a-number.java
@@ -1,18 +1,47 @@
 class Solution {
     public int reachNumber(int target) {
-        if(target < 0) target *= -1; // OR  target = Math.abs(target);
-        int n = 1;
-        int sum = 0;
+        // Linear Search
+        // if(target < 0) target *= -1; // OR  target = Math.abs(target);
+        // int n = 1;
+        // int sum = 0;
 
-        while (sum < target) {
-            sum += n;
-            n++;
+        // while (sum < target) {
+        //     sum += n;
+        //     n++;
+        // }
+
+        // while ((sum - target) % 2 != 0) {
+        //     sum += n;
+        //     n++;
+        // }
+        // return n - 1;
+
+        // Binary Search
+        if(target < 0) target *= -1; // OR  target = Math.abs(target);
+        int pos = 1;
+        long sum = 0;
+        int start = 1;
+        int end = target;
+        int mid;
+        int steps = 0;
+
+        while (start <= end) {
+            mid = start + (end - start)/2; // here mid represents no. of steps
+            sum = (long)mid*(mid+1)/2;
+            if(sum >= target){
+                pos = (int)sum;
+                steps = mid;
+                end = mid - 1;
+            }else{
+                start = mid + 1;
+            }
+
         }
-        
-        while ((sum - target) % 2 != 0) {
-            sum += n;
-            n++;
+
+        while ((pos - target) % 2 != 0) {
+            steps++;
+            pos += steps;
         }
-        return n - 1;
+        return steps;
     }
 }
