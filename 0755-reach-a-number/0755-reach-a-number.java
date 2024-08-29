3544b337119964dc3dc85d69c012a01072ea4600
Rudresh Raj <120813698+RudreshRaj001@users.noreply.github.com>
2024-08-29 21:02:09 +0530

    Time: 1 ms (81.78%), Space: 40.3 MB (53%) - LeetHub

---
+++ 0755-reach-a-number/0755-reach-a-number.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int reachNumber(int target) {
+        if(target < 0) target *= -1;
+        int n = 1;
+        int sum = 0;
+
+        while(sum < target){
+            sum += n;
+            n++;
+        }
+        if((sum - target) % 2 == 0){
+            return n - 1;
+        }else{
+            while((sum - target) % 2 != 0){
+                sum += n;
+                n++;
+            }
+            return n - 1;
+        }
+    }
+}
