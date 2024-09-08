10caf4e0fb8d56bcc55991295ab921c0dedcac2b
Rudresh Raj <120813698+RudreshRaj001@users.noreply.github.com>
2024-09-08 11:29:11 +0530

    Time: 2 ms (78.3%), Space: 45.6 MB (30.17%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int n = nums.length;
+        int ans = 0;
+        for (int i = 0; i < 32; i++) {
+            int cnt = 0;
+            for (int j = 0; j < n; j++) {
+                cnt += (nums[j] >> i) & 1;
+            }
+            if(cnt % 3 == 1) ans = ans | (1<<i);
+        }
+        return ans;
+    }
+}
