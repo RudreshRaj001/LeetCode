c7bfd195ea63034a647b3db2973e6a5bc08064ad
Rudresh Raj <120813698+RudreshRaj001@users.noreply.github.com>
2024-09-08 13:39:51 +0530

    Time: 1 ms (82.28%), Space: 41.6 MB (92.85%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -4,12 +4,12 @@ class Solution {
         result.add(new ArrayList<>());
         result.get(0).add(1); // Initializing the First Row
 
-        for(int i = 1; i < numRows; i++){
-            List<Integer> prevRow = result.get(i-1);
+        for (int i = 1; i < numRows; i++) {
+            List<Integer> prevRow = result.get(i - 1);
             List<Integer> row = new ArrayList<>();
 
             row.add(1); // start the row with 1
-            for(int j = 1; j < prevRow.size(); j++){
+            for (int j = 1; j < prevRow.size(); j++) {
                 row.add(prevRow.get(j - 1) + prevRow.get(j));
             }
 
