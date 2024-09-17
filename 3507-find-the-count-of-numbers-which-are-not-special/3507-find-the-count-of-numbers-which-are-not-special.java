ebdbe2ce7c26005cf748c55679ce32f15fea01e4
Rudresh Raj <120813698+RudreshRaj001@users.noreply.github.com>
2024-09-17 23:39:13 +0530

    Time: 19 ms (69.37%), Space: 44.5 MB (22.09%) - LeetHub

--- 3507-find-the-count-of-numbers-which-are-not-special/3507-find-the-count-of-numbers-which-are-not-special.java
+++ 3507-find-the-count-of-numbers-which-are-not-special/3507-find-the-count-of-numbers-which-are-not-special.java
@@ -1,6 +1,11 @@
 class Solution {
    public static int nonSpecialCount(int l, int r) {
-        List<Integer> primes = sieve((int) Math.sqrt(r));
+        // We need to avoid all the numbers which are Square of a prime numbers 
+        // in range of l to r inclusive 
+        // -> those are special numbers
+        List<Integer> primes = sieve((int) Math.sqrt(r)); 
+        // contains all special from 0 to Math.sqrt(r)
+        // we pass Math.sqrt(r) -> because those are all the prime no.s that matters
         int specialCount = 0;
 
         for (int prime : primes) {
@@ -14,12 +19,13 @@ class Solution {
     }
 
     private static List<Integer> sieve(int n) {
-        boolean[] isPrime = new boolean[n + 1];
+        // Sieve of Eratosthenes
+        boolean[] isPrime = new boolean[n + 1]; // all false
 
         for (int i = 2; i * i <= n; i++) {
             if (!isPrime[i]) {
                 for (int j = i * i; j <= n; j += i) {
-                    isPrime[j] = true;
+                    isPrime[j] = true; // true means non prime
                 }
             }
         }
@@ -27,10 +33,10 @@ class Solution {
         List<Integer> primes = new ArrayList<>();
         for (int i = 2; i <= n; i++) {
             if (!isPrime[i]) {
-                primes.add(i);
+                primes.add(i); // adding primes 
             }
         }
 
-        return primes;
+        return primes; // list of all prime no.s 
     }
 }
