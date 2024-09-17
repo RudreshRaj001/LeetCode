9d6e6d285ba5d43f968dcd9d84c06b629fce3b1b
Rudresh Raj <120813698+RudreshRaj001@users.noreply.github.com>
2024-09-17 22:40:25 +0530

    Time: 19 ms (69.37%), Space: 44.6 MB (22.09%) - LeetHub

--- 3507-find-the-count-of-numbers-which-are-not-special/3507-find-the-count-of-numbers-which-are-not-special.java
+++ 3507-find-the-count-of-numbers-which-are-not-special/3507-find-the-count-of-numbers-which-are-not-special.java
@@ -15,9 +15,6 @@ class Solution {
 
     private static List<Integer> sieve(int n) {
         boolean[] isPrime = new boolean[n + 1];
-        // for (int i = 2; i <= n; i++) {
-        //     isPrime[i] = true;
-        // }
 
         for (int i = 2; i * i <= n; i++) {
             if (!isPrime[i]) {
