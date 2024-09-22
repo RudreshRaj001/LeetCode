d903085c709cfacbf88c3cec9028811d8ed83830
Rudresh Raj <120813698+RudreshRaj001@users.noreply.github.com>
2024-09-22 20:48:52 +0530

    Time: 0 ms (100%), Space: 41.3 MB (82.32%) - LeetHub

--- 0274-h-index/0274-h-index.java
+++ 0274-h-index/0274-h-index.java
@@ -1,22 +1,25 @@
 class Solution {
     public int hIndex(int[] citations) {
         int n = citations.length;
-        int h = 0;
+        int[] buckets = new int[n + 1];
         for (int i = 0; i < n; i++) {
-            h++;
-            int count = 0;
-            for (int j = 0; j < n; j++) {
-                if(citations[j] >= h){
-                    count++;
-                }
-                if(count == h){
-                    break;
-                }
-            }
-            if(h > count){
-                return h - 1;
+            if (citations[i] >= n) {
+                buckets[n]++;
+            } else {
+                buckets[citations[i]]++;
             }
         }
-        return h;
+        // i ---> no. of papers
+        // bucket[i] ---> no. of citations
+
+        int count = 0;
+        for (int i = n; i >= 0; i--) {
+            count += buckets[i];
+            if (count >= i) {
+                // means we have i papers with atleast i citations
+                return i; 
+            }
+        }
+        return 0;
     }
 }
