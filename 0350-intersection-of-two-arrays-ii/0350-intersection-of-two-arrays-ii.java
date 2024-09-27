280559aff23168916656010d4e8cc2c7832c7664
Rudresh Raj <120813698+RudreshRaj001@users.noreply.github.com>
2024-09-27 23:45:32 +0530

    Time: 1 ms (97.83%), Space: 43.2 MB (46.18%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -8,18 +8,12 @@ class Solution {
         
         for(int i = 0; i < n; i++){
                 countArray[nums1[i]]++;
-            // if(countArray[nums1[i]] == 2){
-            //     resList.add(nums1[i]);
-            // }
         }
         for(int i = 0; i < m; i++){
             if(countArray[nums2[i]] >= 1){
                 resList.add(nums2[i]);
                 countArray[nums2[i]]--;
             } 
-            // if(countArray[nums2[i]] == 2){
-            //     resList.add(nums2[i]);
-            // }
         }
 
         // Convert the ArrayList to an array for the return value
