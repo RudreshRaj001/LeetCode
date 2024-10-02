597135bb9b9a12c7dd3e4f7937eae309b315c1db
Rudresh Raj <120813698+RudreshRaj001@users.noreply.github.com>
2024-10-02 21:57:32 +0530

    Time: 7 ms (6.14%), Space: 83.6 MB (99.4%) - LeetHub

--- 2047-find-a-peak-element-ii/2047-find-a-peak-element-ii.java
+++ 2047-find-a-peak-element-ii/2047-find-a-peak-element-ii.java
@@ -4,12 +4,11 @@ class Solution {
         int b = mat[0].length;
         for (int i = 0; i < l; i++) {
             for (int j = 0; j < b; j++) {
-                int top, down, right, left, element;
-                element = mat[i][j];
-                top = i - 1 >= 0 ? mat[i - 1][j] : -1;
-                down = i + 1 < l ? mat[i + 1][j] : -1;
-                left = j - 1 >= 0 ? mat[i][j - 1] : 1;
-                right = j + 1 < b ? mat[i][j + 1] : -1;
+                int element = mat[i][j];
+                int top = i - 1 >= 0 ? mat[i - 1][j] : -1;
+                int down = i + 1 < l ? mat[i + 1][j] : -1;
+                int left = j - 1 >= 0 ? mat[i][j - 1] : 1;
+                int right = j + 1 < b ? mat[i][j + 1] : -1;
 
                 if(element > top && element > down && element > right && element > left){
                     return new int[]{i, j};
