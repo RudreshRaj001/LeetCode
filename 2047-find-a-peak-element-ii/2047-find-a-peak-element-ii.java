514fb1b8dbc7f05fcc2281132fd6c5b48b949501
Rudresh Raj <120813698+RudreshRaj001@users.noreply.github.com>
2024-10-02 21:56:41 +0530

    Time: 7 ms (6.14%), Space: 83.3 MB (99.77%) - LeetHub

--- 2047-find-a-peak-element-ii/2047-find-a-peak-element-ii.java
+++ 2047-find-a-peak-element-ii/2047-find-a-peak-element-ii.java
@@ -2,39 +2,14 @@ class Solution {
     public int[] findPeakGrid(int[][] mat) {
         int l = mat.length;
         int b = mat[0].length;
-        /*
-
-          00 01 02
-          10 11 12
-          20 21 22
-
-         */
         for (int i = 0; i < l; i++) {
             for (int j = 0; j < b; j++) {
                 int top, down, right, left, element;
                 element = mat[i][j];
-                if(i - 1 >= 0){
-                    top = mat[i - 1][j];
-                }else{
-                    top = -1;
-                }
-                if(i + 1 < l){
-                    down = mat[i + 1][j];
-                }else{
-                    down = -1;
-                }
-
-                if(j - 1 >= 0){
-                    left = mat[i][j - 1];
-                }else{
-                    left = -1;
-                }
-
-                if(j + 1 < b){
-                    right = mat[i][j + 1];
-                }else{
-                    right = -1;
-                }
+                top = i - 1 >= 0 ? mat[i - 1][j] : -1;
+                down = i + 1 < l ? mat[i + 1][j] : -1;
+                left = j - 1 >= 0 ? mat[i][j - 1] : 1;
+                right = j + 1 < b ? mat[i][j + 1] : -1;
 
                 if(element > top && element > down && element > right && element > left){
                     return new int[]{i, j};
