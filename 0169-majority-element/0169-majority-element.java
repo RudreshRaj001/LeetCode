42a17cc41ad161e35cb29da592605c37378688e7
Rudresh Raj <120813698+RudreshRaj001@users.noreply.github.com>
2024-10-04 22:54:03 +0530

    Time: 1 ms (99.8%), Space: 53 MB (51.02%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,5 +1,8 @@
 class Solution {
     public int majorityElement(int[] nums) {
+        // Moore's voting algorithm :-
+        // as the majority element appeared more than [n/2] times 
+        // means its count will cancel out all the rest of the elements counts combined.
         int n = nums.length;
         int count = 0;
         int candidate = 0;
