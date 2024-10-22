918caa3c7b0e39841eb90fde6e393ba17e849aba
Rudresh Raj <120813698+RudreshRaj001@users.noreply.github.com>
2024-10-22 13:42:03 +0530

    Time: 79 ms (5.24%), Space: 46 MB (33.7%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -5,12 +5,12 @@ class Solution {
         for (int i = 0; i < len; i++) {
             if(i > 0 && i < len - zeros && nums[i-1] == 0) i--;
             if (nums[i] == 0) {
-                zeros++;
-                for (int j = i; j < len - 1; j++) {
+                for (int j = i; j < len - 1 - zeros; j++) {
                     int temp = nums[j];
                     nums[j] = nums[j + 1];
                     nums[j + 1] = temp;
                 }
+                zeros++;
             }
         }
     }
