7edaba7209f1a7101d8b01cc6d7d1d709b9c5d4b
Rudresh Raj <120813698+RudreshRaj001@users.noreply.github.com>
2024-10-25 14:12:09 +0530

    Time: 7 ms (65.31%), Space: 47.8 MB (54.97%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,15 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for(String word : strs){
+            char[] chars = word.toCharArray();
+            Arrays.sort(chars);
+            String newWord = new String(chars);
+            if(!map.containsKey(newWord)){
+                map.put(newWord, new ArrayList<>());
+            }
+            map.get(newWord).add(word);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
