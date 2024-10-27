22b00a4cad2f2a7cbe6868007bab408be54f97a9
Rudresh Raj <120813698+RudreshRaj001@users.noreply.github.com>
2024-10-27 11:28:49 +0530

    Time: 2 ms (99.22%), Space: 48.2 MB (20.16%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,20 +1,41 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
+        // Solution Using Boyer-Moore Majority Vote algorithm
         List<Integer> ans = new ArrayList<>();
-        HashMap<Integer, Integer> map = new HashMap<>();
+        int count1 = 0;
+        int count2 = 0;
+        int candidate1 = 0;
+        int candidate2 = 1;
         int n = nums.length;
-        for(int i = 0; i < n; i++){
-            if(map.containsKey(nums[i])){
-                map.put(nums[i], map.get(nums[i]) + 1);
+        for(int num : nums){
+            if(num == candidate1){
+                count1++;
+            }else if(num == candidate2){
+                count2++;
+            }else if(count1 == 0){
+                candidate1 = num;
+                count1 = 1;
+            }else if(count2 == 0){
+                candidate2 = num;
+                count2 = 1;
             }else{
-                map.put(nums[i], 1);
+                count1--;
+                count2--;
             }
         }
+        
+        count1 = 0;
+        count2 = 0;
+        for (int num : nums) {
+            if (num == candidate1) count1++;
+            else if (num == candidate2) count2++;
+        }
 
-        for(int key : map.keySet()){
-            if(map.get(key) > n/3){
-                ans.add(key);
-            }
+        if(count1 > n/3){
+            ans.add(candidate1);
+        }
+        if(count2 > n/3){
+            ans.add(candidate2);
         }
         return ans;
     }
