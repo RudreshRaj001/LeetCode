7e9b5671028582b15393b4a13b1cef2b1a9e00e3
Rudresh Raj <120813698+RudreshRaj001@users.noreply.github.com>
2024-11-02 14:37:54 +0530

    Time: 1 ms (49.21%), Space: 41.7 MB (18.11%) - LeetHub

---
+++ 0500-keyboard-row/0500-keyboard-row.java
@@ -0,0 +1,79 @@
+class Solution {
+    public String[] findWords(String[] words) {
+        String first = "qwertyuiop";
+        String firstCap = "QWERTYUIOP";
+
+        String second = "asdfghjkl";
+        String secondCap = "ASDFGHJKL";
+
+        String third = "zxcvbnm";
+        String thirdCap = "ZXCVBNM";
+
+        HashSet<Character> set1 = new HashSet<>();
+        HashSet<Character> set2 = new HashSet<>();
+        HashSet<Character> set3 = new HashSet<>();
+
+        for(int i = 0; i < 10;i++){
+            set1.add(first.charAt(i));
+            set1.add(firstCap.charAt(i));
+        }
+
+        for(int i = 0; i < 9;i++){
+            set2.add(second.charAt(i));
+            set2.add(secondCap.charAt(i));
+        }
+
+        for(int i = 0; i < 7;i++){
+            set3.add(third.charAt(i));
+            set3.add(thirdCap.charAt(i));
+        }
+
+        ArrayList<String> arraylist = new ArrayList<String>();
+        for(int i = 0; i < words.length; i++){
+            String temp = words[i];
+            boolean status = false;
+
+            if(set1.contains(temp.charAt(0))){
+               for(int j = 0; j < temp.length(); j++){
+
+                if(!set1.contains(temp.charAt(j))){
+                    status = true;
+                    break;
+                }
+
+            } 
+            }else if(set2.contains(temp.charAt(0))){
+                for(int j = 0; j < temp.length(); j++){
+
+                if(!set2.contains(temp.charAt(j))){
+                    status = true;
+                    break;
+                }
+
+            }
+            }else{
+                for(int j = 0; j < temp.length(); j++){
+
+                    if(!set3.contains(temp.charAt(j))){
+                        status = true;
+                        break;
+                    }
+
+               }
+            }
+            // for(int j = 0; j < temp.length(); j++){
+
+            //     if(!set.contains(temp.charAt(j))){
+            //         status = true;
+            //         break;
+            //     }
+
+            // }
+            if(!status){
+                arraylist.add(temp);
+            }
+        }
+
+        return arraylist.toArray(new String[0]);
+    }
+}
