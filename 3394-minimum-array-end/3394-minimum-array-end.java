79a0f8120c6d4a52afb4366304a8e82601bc6e7b
Rudresh Raj <120813698+RudreshRaj001@users.noreply.github.com>
2024-11-09 12:03:57 +0530

    Time: 1 ms (87.5%), Space: 40.9 MB (45.19%) - LeetHub

--- 3394-minimum-array-end/3394-minimum-array-end.java
+++ 3394-minimum-array-end/3394-minimum-array-end.java
@@ -1,11 +1,39 @@
 class Solution {
     public long minEnd(int n, int x) {
-        long first = x;
-        while(n > 1){
-            first++;
-            first = x | first;
-            n--;
+        // long res = x;
+
+        // int i_x = 1;
+        // int i_n = 1;
+        // // x = 101001010101
+        // // n - 1 = 010010101011
+        // while(i_n < n){
+        //     if((i_x & x) == 0){
+        //         if((i_n & (n - 1)) == 1){
+        //             res = res | i_x;
+        //         }
+        //         i_n = i_n << 1;
+        //     }
+        //     i_x = i_x << 1;
+        // }
+
+        
+        // return res;
+
+        long res = x;
+
+        long i_x = 1;
+        long i_n = n - 1;
+        // x = 101001010101
+        // n - 1 = 010010101011
+        while(i_n != 0){
+            if((i_x & x) == 0){
+                res = res | (i_n & 1)*i_x;
+                i_n = i_n >> 1;
+            }
+            i_x = i_x << 1;
         }
-        return first;
+
+        
+        return res;
     }
 }
