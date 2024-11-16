3ccfea3f1128c41726ce5a4e3d3f5c11440cf797
Rudresh Raj <120813698+RudreshRaj001@users.noreply.github.com>
2024-11-16 11:46:47 +0530

    Time: 1 ms (100%), Space: 45.5 MB (27.66%) - LeetHub

--- 3522-find-the-power-of-k-size-subarrays-i/3522-find-the-power-of-k-size-subarrays-i.java
+++ 3522-find-the-power-of-k-size-subarrays-i/3522-find-the-power-of-k-size-subarrays-i.java
@@ -1,24 +1,43 @@
 class Solution {
     public int[] resultsArray(int[] nums, int k) {
+        if(k == 1) return nums;
         int n = nums.length;
         int[] ans = new int[n - k + 1]; 
         
-        for (int i = 0; i <= n - k; i++) {
-            boolean isConsecutive = true;
-            int maxElement = nums[i];
+        // for (int i = 0; i <= n - k; i++) {
+            // boolean isConsecutive = true;
+            // int maxElement = nums[i];
 
-            for (int j = i; j < i + k - 1; j++) {
-                if (nums[j] + 1 != nums[j + 1]) {
-                    isConsecutive = false;
-                    break;
-                }
-                maxElement = Math.max(maxElement, nums[j + 1]);
+            // for (int j = i; j < i + k - 1; j++) {
+            //     if (nums[j] + 1 != nums[j + 1]) {
+            //         isConsecutive = false;
+            //         break;
+            //     }
+            //     maxElement = Math.max(maxElement, nums[j + 1]);
+            // }
+            
+            // if (isConsecutive) {
+            //     ans[i] = maxElement;
+            // } else {
+            //     ans[i] = -1;
+            // }
+        // }
+        int count = 1;
+        int j = 0;
+        for(int i = 1; i < n; i++){
+            if(nums[i - 1] + 1 == nums[i]){
+                count++;
+            }else{
+                count = 1;
             }
             
-            if (isConsecutive) {
-                ans[i] = maxElement;
-            } else {
-                ans[i] = -1;
+            if(i >= k - 1){
+                if(count >= k){
+                    ans[j] = nums[i];
+                }else{
+                    ans[j] = -1;
+                }
+                j++;
             }
         }
         
