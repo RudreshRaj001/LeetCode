2db5ee2b6a5282e8ea44754c13162b99b7bcb571
Rudresh Raj <120813698+RudreshRaj001@users.noreply.github.com>
2024-11-17 15:08:29 +0530

    Time: 8 ms (20.72%), Space: 45.5 MB (7%) - LeetHub

--- 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.java
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.java
@@ -15,14 +15,15 @@ class Solution {
             int unLen = unprocessed.length();
             int diff = unLen - partLen;
             if (diff < 0) {
-                return skipApple("",unprocessed + str.substring(partLen),part,partLen);
+                return skipApple("", unprocessed + str.substring(partLen), part, partLen);
             }
-            String Splitfr = unprocessed.substring(0, diff);// first part of the string;
-            String SplitSc = unprocessed.substring(diff);
-            return skipApple(Splitfr, SplitSc + str.substring(partLen),part,partLen);
+            // Splitting Unprocessed String
+            String Splitfr = unprocessed.substring(0, diff); // first part;
+            String SplitSc = unprocessed.substring(diff); // first part;
+            return skipApple(Splitfr, SplitSc + str.substring(partLen), part, partLen);
         } else {
             unprocessed += ch;
-            return skipApple(unprocessed ,str.substring(1),part,partLen);
+            return skipApple(unprocessed, str.substring(1), part, partLen);
         }
     }
 }
