3b103ea7142148370a76977314c8204fd0910ef3
Rudresh Raj <120813698+RudreshRaj001@users.noreply.github.com>
2024-11-17 15:15:39 +0530

    Time: 8 ms (20.72%), Space: 45.2 MB (7%) - LeetHub

--- 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.java
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.java
@@ -1,10 +1,29 @@
 class Solution {
-    public String removeOccurrences(String s, String part) {
-        int index = s.indexOf(part);
-        while(index != -1) {
-            s = s.substring(0, index) + s.substring(index + part.length());
-            index = s.indexOf(part);
+    public String removeOccurrences(String up, String part) {
+        int partLen = part.length();
+        return skip("", up, part, partLen);
+    }
+
+    static String skip(String unprocessed, String str, String part, int partLen) {
+        if (str.isEmpty()) {
+            return unprocessed;
         }
-        return s;
+
+        char ch = str.charAt(0);
+
+        if (str.startsWith(part)) {
+            int unLen = unprocessed.length();
+            int diff = unLen - partLen;
+            if (diff < 0) {
+                return skip("", unprocessed + str.substring(partLen), part, partLen);
+            }
+            // Splitting Unprocessed String
+            String Splitfr = unprocessed.substring(0, diff); // first part;
+            String SplitSc = unprocessed.substring(diff); // first part;
+            return skip(Splitfr, SplitSc + str.substring(partLen), part, partLen);
+        } else {
+            unprocessed += ch;
+            return skip(unprocessed, str.substring(1), part, partLen);
+        }
     }
 }
