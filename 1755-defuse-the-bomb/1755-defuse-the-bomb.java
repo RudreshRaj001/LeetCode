b078434746ddeaba1b9e653ac79f28459438b174
Rudresh Raj <120813698+RudreshRaj001@users.noreply.github.com>
2024-11-19 10:48:50 +0530

    Time: 0 ms (100%), Space: 42.3 MB (24.36%) - LeetHub

--- 1755-defuse-the-bomb/1755-defuse-the-bomb.java
+++ 1755-defuse-the-bomb/1755-defuse-the-bomb.java
@@ -3,19 +3,30 @@ class Solution {
         int n = code.length;
         int[] res = new int[n];
         if(k == 0) return res;
-        for(int i = 0; i < n; i++){
-            int sum = 0;
-            if(k > 0){
-                for(int j = i + 1; j <= i + k; j++){
-                    sum += code[j%n];
-                }
-            }else{
-                for(int j = i - 1; j > i - 1 - Math.abs(k); j--){
-                    sum += code[(j % n + n) % n];
-                }
+
+        int sum = 0;
+        if(k > 0){
+             for (int i = 1; i <= k; i++) {
+                sum += code[i % n];
             }
-            res[i] = sum;
+
+            for (int i = 0; i < n; i++) {
+                res[i] = sum;
+                sum -= code[(i + 1) % n]; // Remove the next element going out of the window
+                sum += code[(i + k + 1) % n]; // Add the next element coming into the window
+            }
+        }else{
+            for(int i = -1; i >= k; i--){
+                sum += code[(i + n)%n];
+            }
+
+            for(int i = 0; i < n; i++){
+                res[i] = sum;
+                sum += code[i];
+                sum -= code[(i + k + n)%n];
+            }
         }
+
         return res;
     }
 }
