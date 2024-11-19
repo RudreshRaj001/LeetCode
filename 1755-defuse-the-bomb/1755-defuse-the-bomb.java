480346420083482ae64c4d728ed2f4538035b8c9
Rudresh Raj <120813698+RudreshRaj001@users.noreply.github.com>
2024-11-19 11:08:33 +0530

    Time: 1 ms (70.93%), Space: 42.4 MB (24.36%) - LeetHub

--- 1755-defuse-the-bomb/1755-defuse-the-bomb.java
+++ 1755-defuse-the-bomb/1755-defuse-the-bomb.java
@@ -2,28 +2,25 @@ class Solution {
     public int[] decrypt(int[] code, int k) {
         int n = code.length;
         int[] res = new int[n];
-        if(k == 0) return res;
+        if (k == 0)
+            return res;
 
+        int l = 0;
         int sum = 0;
-        if(k > 0){
-             for (int i = 1; i <= k; i++) {
-                sum += code[i % n];
+
+        for (int i = 0; i < n + Math.abs(k); i++) {
+            sum += code[i % n];
+            if (i - l + 1 > Math.abs(k)) {
+                sum -= code[l % n];
+                l = (l + 1) % n;
             }
 
-            for (int i = 0; i < n; i++) {
-                res[i] = sum;
-                sum -= code[(i + 1) % n]; // Remove the next element going out of the window
-                sum += code[(i + k + 1) % n]; // Add the next element coming into the window
-            }
-        }else{
-            for(int i = -1; i >= k; i--){
-                sum += code[(i + n)%n];
-            }
-
-            for(int i = 0; i < n; i++){
-                res[i] = sum;
-                sum += code[i];
-                sum -= code[(i + k + n)%n];
+            if (i - l + 1 == Math.abs(k)) {
+                if (k > 0) {
+                    res[(l - 1 + n) % n] = sum;
+                } else {
+                    res[(i + 1) % n] = sum;
+                }
             }
         }
 
