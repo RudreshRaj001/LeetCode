dfb7ffa27d2e4071df821755e560c55c857442c5
Rudresh Raj <120813698+RudreshRaj001@users.noreply.github.com>
2024-11-26 18:55:34 +0530

    Time: 0 ms (100%), Space: 46 MB (7.06%) - LeetHub

--- 3188-find-champion-i/3188-find-champion-i.java
+++ 3188-find-champion-i/3188-find-champion-i.java
@@ -1,18 +1,23 @@
 class Solution {
     public int findChampion(int[][] grid) {
         int n = grid.length;
+        int winner = 0;
         for(int i = 0; i < n; i++){
-            boolean flag = true;
-            for(int j = 0; j < n;j++){
-                if(i == j) continue;
-                if(grid[i][j] != 1){
-                    flag = false;
-                    break;
-                }
+            // boolean flag = true;
+            // for(int j = 0; j < n;j++){
+            //     if(i == j) continue;
+            //     if(grid[i][j] != 1){
+            //         flag = false;
+            //         break;
+            //     }
+            // }
+            // if(flag) return i;
+            if(winner == i) continue;
+            if(grid[winner][i] == 0){
+                winner = i;
             }
-            if(flag) return i;
         }
     
-        return -1;
+        return winner;
     }
 }
