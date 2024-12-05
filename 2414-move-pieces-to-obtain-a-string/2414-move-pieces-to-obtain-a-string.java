72aba40b0d07eaacf542a7e9343853b6436cf070
Rudresh Raj <120813698+RudreshRaj001@users.noreply.github.com>
2024-12-05 20:13:04 +0530

    Time: 15 ms (66.47%), Space: 46.2 MB (16.67%) - LeetHub

--- 2414-move-pieces-to-obtain-a-string/2414-move-pieces-to-obtain-a-string.java
+++ 2414-move-pieces-to-obtain-a-string/2414-move-pieces-to-obtain-a-string.java
@@ -4,9 +4,11 @@ class Solution {
         int j = 0;
         int n = start.length();
         while(i <= n && j <= n){
-            while(i < n && start.charAt(i) == '_') i++;
+            while(i < n && start.charAt(i) == '_') i++; // letter
             while(j < n && target.charAt(j) == '_') j++;
+
             if(i == n || j == n) return i == n && j == n;
+
             if(start.charAt(i) == target.charAt(j)){
                 if(start.charAt(i) == 'L' && i < j) return false;
                 if(start.charAt(i) == 'R' && i > j) return false;
