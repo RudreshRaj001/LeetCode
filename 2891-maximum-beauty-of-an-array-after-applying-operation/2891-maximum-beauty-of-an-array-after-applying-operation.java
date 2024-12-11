ba7cdff48596583be703ded08b5b692625ef36e9
Rudresh Raj <120813698+RudreshRaj001@users.noreply.github.com>
2024-12-11 20:15:33 +0530

    Time: 39 ms (75.27%), Space: 60.1 MB (61.39%) - LeetHub

--- 2891-maximum-beauty-of-an-array-after-applying-operation/2891-maximum-beauty-of-an-array-after-applying-operation.java
+++ 2891-maximum-beauty-of-an-array-after-applying-operation/2891-maximum-beauty-of-an-array-after-applying-operation.java
@@ -1,28 +1,26 @@
 class Solution {
     public int maximumBeauty(int[] nums, int k) {
         Arrays.sort(nums);
-        // int left = 0;
-        // int right = 0;
-        // int ans = right - left + 1;
-        // while(right < nums.length){
-        //     if(nums[right] <= nums[left] + (2*k)){
-        //         right = binarySearch(nums, nums[left] + (2*k), false);
-        //     }else{
-        //         left++;
-        //     }
-        //     ans = Math.max(right - left, ans);
-        // }
-        int ans = 0;
+        int left = 0;
         int right = 0;
-        for(int left = 0; left < nums.length; left++){
-            right = binarySearch(nums, nums[left] + (2 * k), right);
-            ans = Math.max(ans, right - left + 1);
+        int ans = right - left + 1;
+        while(right < nums.length){
+            if(nums[right] <= nums[left] + (2*k)){
+                right++;
+            }else{
+                left++;
+            }
+            ans = Math.max(right - left, ans);
         }
+        // int ans = 0;
+        // int right = 0;
+        // for(int left = 0; left < nums.length; left++){
+        //     right = binarySearch(nums, nums[left] + (2 * k), right);
+        //     ans = Math.max(ans, right - left + 1);
+        // }
         return ans;
     }
     private int binarySearch(int[] nums, int target,int start){
-        // flag -> true -> returns floor
-        // flag -> false -> returns ceil
         int l = start;
         int r = nums.length - 1;
         while(l <= r){
