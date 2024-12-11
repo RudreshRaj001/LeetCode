b1926061c6d56fc5b01d349fe9efca7581661980
Rudresh Raj <120813698+RudreshRaj001@users.noreply.github.com>
2024-12-11 20:16:08 +0530

    Time: 39 ms (75.27%), Space: 60.9 MB (19.31%) - LeetHub

--- 2891-maximum-beauty-of-an-array-after-applying-operation/2891-maximum-beauty-of-an-array-after-applying-operation.java
+++ 2891-maximum-beauty-of-an-array-after-applying-operation/2891-maximum-beauty-of-an-array-after-applying-operation.java
@@ -12,25 +12,6 @@ class Solution {
             }
             ans = Math.max(right - left, ans);
         }
-        // int ans = 0;
-        // int right = 0;
-        // for(int left = 0; left < nums.length; left++){
-        //     right = binarySearch(nums, nums[left] + (2 * k), right);
-        //     ans = Math.max(ans, right - left + 1);
-        // }
         return ans;
-    }
-    private int binarySearch(int[] nums, int target,int start){
-        int l = start;
-        int r = nums.length - 1;
-        while(l <= r){
-            int mid = l + (r - l) / 2;
-            if(nums[mid] <= target){
-                l = mid + 1;
-            }else{
-                r = mid - 1;
-            }
-        }
-        return r;
     }
 }
