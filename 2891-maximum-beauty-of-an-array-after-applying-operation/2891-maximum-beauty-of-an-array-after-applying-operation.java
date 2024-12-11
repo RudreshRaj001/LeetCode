36368ac33582404d17de4f9ce901625b77a4967d
Rudresh Raj <120813698+RudreshRaj001@users.noreply.github.com>
2024-12-11 20:10:39 +0530

    Time: 93 ms (15.38%), Space: 60.5 MB (35.64%) - LeetHub

--- 2891-maximum-beauty-of-an-array-after-applying-operation/2891-maximum-beauty-of-an-array-after-applying-operation.java
+++ 2891-maximum-beauty-of-an-array-after-applying-operation/2891-maximum-beauty-of-an-array-after-applying-operation.java
@@ -1,35 +1,37 @@
 class Solution {
     public int maximumBeauty(int[] nums, int k) {
         Arrays.sort(nums);
-        int left = 0;
-        int right = 0;
-        int ans = right - left + 1;
-        while(right < nums.length){
-            if(nums[right] <= nums[left] + (2*k)){
-                right++;
-            }else{
-                left++;
-            }
-            ans = Math.max(right - left, ans);
+        // int left = 0;
+        // int right = 0;
+        // int ans = right - left + 1;
+        // while(right < nums.length){
+        //     if(nums[right] <= nums[left] + (2*k)){
+        //         right = binarySearch(nums, nums[left] + (2*k), false);
+        //     }else{
+        //         left++;
+        //     }
+        //     ans = Math.max(right - left, ans);
+        // }
+        int ans = 0;
+        for(int left = 0; left < nums.length; left++){
+            int right = binarySearch(nums, nums[left] + (2 * k));
+            ans = Math.max(ans, right - left + 1);
         }
         return ans;
-
     }
-    private int binarySearch(int[] nums, int target, boolean flag){
+    private int binarySearch(int[] nums, int target){
         // flag -> true -> returns floor
         // flag -> false -> returns ceil
         int l = 0;
         int r = nums.length - 1;
         while(l <= r){
             int mid = l + (r - l) / 2;
-            if(nums[mid] == target){
-                return mid;
-            }else if(nums[mid] < target){
+            if(nums[mid] <= target){
                 l = mid + 1;
             }else{
                 r = mid - 1;
             }
         }
-        return flag ? r : l;
+        return r;
     }
 }
