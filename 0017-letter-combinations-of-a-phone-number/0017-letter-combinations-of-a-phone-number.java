0a9803482c84bcdfc65c77baa71ab2d27f3f1c7e
Rudresh Raj <120813698+RudreshRaj001@users.noreply.github.com>
2024-12-29 21:18:06 +0530

    Time: 6 ms (7.7%), Space: 42.7 MB (6.54%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -6,36 +6,36 @@ class Solution {
         return list;
     }
     static ArrayList<String> helper(String p, String up){
-    if(up.isEmpty()){
-      ArrayList<String> result = new ArrayList<>();
-      result.add(p);
-      return result;
-    }
-    int digit = up.charAt(0) - '0'; // this will convert '2' to 2
-    ArrayList<String> result = new ArrayList<>();
+        if(up.isEmpty()){
+        ArrayList<String> result = new ArrayList<>();
+        result.add(p);
+        return result;
+        }
+        int digit = up.charAt(0) - '0'; // this will convert '2' to 2
+        ArrayList<String> result = new ArrayList<>();
 
-    // int i = (digit - 1) * 3;
-    if(digit == 7){
-        for(int i = (digit - 2) * 3; i <= ((digit - 1) * 3) ; i++){
-            char ch = (char) ('a' + i);
-            result.addAll(helper(p + ch, up.substring(1)));
-        } 
-    }else if(digit == 8){
-        for(int i = (digit - 2) * 3 + 1; i <= (digit - 1) * 3; i++){
-            char ch = (char) ('a' + i);
-            result.addAll(helper(p + ch, up.substring(1)));
-        } 
-    }else if(digit == 9){
-        for(int i = (digit - 2) * 3 + 1; i < (digit * 3) - 1; i++){
-            char ch = (char) ('a' + i);
-            result.addAll(helper(p + ch, up.substring(1)));
-        } 
-    }else{
-        for(int i = (digit - 2) * 3;i < (digit - 1) * 3; i++){
-            char ch = (char) ('a' + i);
-            result.addAll(helper(p + ch, up.substring(1)));
-        } 
-    }
-    return result;
+        // int i = (digit - 1) * 3;
+        if(digit == 7){
+            for(int i = (digit - 2) * 3; i <= ((digit - 1) * 3) ; i++){
+                char ch = (char) ('a' + i);
+                result.addAll(helper(p + ch, up.substring(1)));
+            } 
+        }else if(digit == 8){
+            for(int i = (digit - 2) * 3 + 1; i <= (digit - 1) * 3; i++){
+                char ch = (char) ('a' + i);
+                result.addAll(helper(p + ch, up.substring(1)));
+            } 
+        }else if(digit == 9){
+            for(int i = (digit - 2) * 3 + 1; i < (digit * 3) - 1; i++){
+                char ch = (char) ('a' + i);
+                result.addAll(helper(p + ch, up.substring(1)));
+            } 
+        }else{
+            for(int i = (digit - 2) * 3;i < (digit - 1) * 3; i++){
+                char ch = (char) ('a' + i);
+                result.addAll(helper(p + ch, up.substring(1)));
+            } 
+        }
+        return result;
   }
 }
