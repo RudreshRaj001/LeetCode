67fa9edec9b28a83f704cf307ebb588876628089
Rudresh Raj <120813698+RudreshRaj001@users.noreply.github.com>
2025-01-04 21:27:10 +0530

    Time: 287 ms (20.68%), Space: 46.2 MB (20.68%) - LeetHub

--- 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.java
+++ 2059-unique-length-3-palindromic-subsequences/2059-unique-length-3-palindromic-subsequences.java
@@ -9,11 +9,11 @@ class Solution {
             first[s.charAt(i) - 'a'] = Math.min(first[s.charAt(i) - 'a'], i);
             last[s.charAt(i) - 'a'] = i;
         }
-        for (int i = 0; i < 26; i++){
-            if (first[i] < last[i]){
+        for (int i = 0; i < 26; i++) {
+            if (first[i] < last[i]) {
                 res += s.substring(first[i] + 1, last[i]).chars().distinct().count();
             }
         }
-    return res;
+        return res;
     }
 }
