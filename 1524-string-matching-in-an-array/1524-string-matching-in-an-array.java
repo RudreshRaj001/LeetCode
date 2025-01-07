04d2747e06f790d565dbcdeadd6be2ae54f73be3
Rudresh Raj <120813698+RudreshRaj001@users.noreply.github.com>
2025-01-07 11:41:23 +0530

    Time: 4 ms (86.21%), Space: 42.8 MB (8.27%) - LeetHub

--- 1524-string-matching-in-an-array/1524-string-matching-in-an-array.java
+++ 1524-string-matching-in-an-array/1524-string-matching-in-an-array.java
@@ -6,6 +6,7 @@ class Solution {
             for(int j = 0; j < n; j++){
                 if(i != j && words[j].contains(words[i])){
                     ans.add(words[i]);
+                    break;
                 }
             }
         }
