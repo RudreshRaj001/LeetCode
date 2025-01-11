f82df45cc491db29f84862f211ec683c397eac0c
Rudresh Raj <120813698+RudreshRaj001@users.noreply.github.com>
2025-01-11 08:12:58 +0530

    Time: 11 ms (37.77%), Space: 45.6 MB (32.37%) - LeetHub

--- 1502-construct-k-palindrome-strings/1502-construct-k-palindrome-strings.java
+++ 1502-construct-k-palindrome-strings/1502-construct-k-palindrome-strings.java
@@ -1,12 +1,14 @@
 class Solution {
     public boolean canConstruct(String s, int k) {
-        int odd = 0;
         int n = s.length();
+        if(n < k) return false;
+        if(n == k) return true;
+        int odd = 0;
         int[] count = new int[26];
         for(int i = 0; i < n; i++){
             count[s.charAt(i) - 'a'] ^= 1;
             odd += count[s.charAt(i) - 'a'] == 0 ? -1 : 1;
         }
-        return odd <= k && k <= n;
+        return odd <= k;
     }
 }
