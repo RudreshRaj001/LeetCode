0866d99d6faad152b591cbc7d4ca9f394b322035
Rudresh Raj <120813698+RudreshRaj001@users.noreply.github.com>
2025-01-16 12:54:54 +0530

    Time: 0 ms (100%), Space: 44.7 MB (31%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -18,41 +18,19 @@ class Solution {
         if (root == null) {
             return true;
         }
-
-        // Calculate the height of left and right subtrees
-        int leftHeight = Depth(root.left);
-        int rightHeight = Depth(root.right);
-
-        // Check if the absolute difference in heights
-        // of left and right subtrees is <= 1
-        if (Math.abs(leftHeight - rightHeight) <= 1 &&
-            isBalanced(root.left) &&
-            isBalanced(root.right)) {
-            return true;
-        }
-
-        return false;
+        return h(root) != -1;
     }
-     public int Depth(TreeNode root) {
-        if(root == null) return 0;
-        int depth = 1;
-        int left = 0;
-        int right = 0;
-        if(root.left != null){
-            left = travel(root.left, depth);
+    private int h(TreeNode root){
+        if (root == null) {
+            return 0;
         }
-        if(root.right != null){
-            right = travel(root.right, depth);
-        }
-        depth = Math.max(Math.max(left, right), depth);
-        return depth;
-    }
-    private int travel(TreeNode node, int deep){
-        if(node == null) return deep;
-        deep++;
-        int left = travel(node.left, deep);
-        int right = travel(node.right, deep);
-        deep = Math.max(left, right);
-        return deep;
+        int rh = h(root.right);
+        int lh = h(root.left);
+        if(lh == -1 || rh == -1) return -1;
+        if(Math.abs(lh - rh) > 1) return -1;
+        return Math.max(rh , lh) + 1;
     }
 }
+
+
+  
