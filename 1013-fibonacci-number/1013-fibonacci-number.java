3b1dbd1f9b37d3b3aeea6cb6fb23313520ebddf2
Rudresh Raj <120813698+RudreshRaj001@users.noreply.github.com>
2025-02-19 12:13:14 +0530

    Time: 0 ms (100%), Space: 40.3 MB (78.84%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.java
+++ 1013-fibonacci-number/1013-fibonacci-number.java
@@ -2,10 +2,14 @@ class Solution {
     public int fib(int n) {
         if(n == 0) return 0;
         if(n == 1) return 1;
+
         int[] dp = new int[n + 1];
+
         Arrays.fill(dp, -1);
+
         dp[0] = 0;
         dp[1] = 1;
+        
         return helper(n, dp);
     }
     private int helper(int n, int[] dp){
