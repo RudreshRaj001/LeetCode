c265cc45c61d76d59729129e9408ad4156388da4
Rudresh Raj <120813698+RudreshRaj001@users.noreply.github.com>
2025-03-30 23:10:53 +0530

    Time: 6 ms (57.25%), Space: 42.1 MB (80.49%) - LeetHub

--- 0768-partition-labels/0768-partition-labels.java
+++ 0768-partition-labels/0768-partition-labels.java
@@ -5,26 +5,20 @@ class Solution {
         List<Integer> ans = new ArrayList<>();
         int st = 0;
         int e = 0;
-
-
         int[] arr = new int[26];
         for(int i = 0; i < s.length(); i++){
             arr[s.charAt(i) - 'a'] = i;
         }
-
         int i = 0;
         while(i < s.length()){
             char ch = s.charAt(i);
             e = Math.max(e, arr[s.charAt(i) - 'a']);
-
             if(i == e){
                 ans.add(e - st + 1);
                 st = e + 1;
             }
-
             i++;
         }
-
         return ans;
     }
 }
