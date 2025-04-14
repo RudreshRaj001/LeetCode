5016ef85b48eb17c7ccaa71c45fe44b69dc77e3e
Rudresh Raj <120813698+RudreshRaj001@users.noreply.github.com>
2025-04-14 23:39:58 +0530

    Time: 6 ms (83.27%), Space: 57.2 MB (49.21%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.java
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.java
@@ -1,53 +1,15 @@
 class Solution {
-    // Cyclic SOrt
-    // public List<Integer> findDuplicates(int[] a) {
-    //     int i = 0;
-    //     int n = a.length;
-    //     while (i < n) {
-    //         int correctIndex = a[i] - 1;
-    //         if (a[i] != a[correctIndex]) {
-    //             // swap 
-    //             swap(a, i, correctIndex);
-    //         } else {
-    //             i++;
-    //         }
-    //     }
 
-    //     List<Integer> List = new ArrayList<>();
-    //     for (int j = 0; j < n; j++) {
-    //         if (a[j] != j + 1) {
-    //             List.add(a[j]);
-    //         }
-    //     }
-
-    //     return List; 
-    // }
-
-    // void swap(int[] a, int i, int j) {
-    //     int temp = a[i];
-    //     a[i] = a[j];
-    //     a[j] = temp;
-    // }
-
-
-    // Best Method - Count Sort
     public List<Integer> findDuplicates(int[] nums) {
         int n = nums.length;
-        // int[] count = new int[n + 1];
         List<Integer> List = new ArrayList<>();
         for (int i = 0; i < n; i++) {
-            // count[nums[i]]++;
             int index = Math.abs(nums[i]) - 1;
             if (nums[index] > 0) {
                 nums[index] *= -1;
             } else {
-                // return index + 1;
-                
                 List.add(Math.abs(nums[i]));
             }
-            // if (count[nums[i]] > 1) {
-                
-            // }
         }
         return List;
     }
