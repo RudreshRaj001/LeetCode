f2275930fdf847783d7e91db094ec4de1eff9868
Rudresh Raj <120813698+RudreshRaj001@users.noreply.github.com>
2025-04-18 20:43:54 +0530

    Time: 13 ms (26.07%), Space: 45.8 MB (47.18%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.java
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -1,21 +1,19 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
         HashSet<Integer> set = new HashSet<>();
-        int sum = 0;
         int i = 1;
         int legalsum = 0;
         int dupli = -1;
         for(int n : nums){
-            legalsum += i;
+            legalsum += i - n;
             i++;
             if(set.contains(n)){
                 dupli = n;
             }else{
                 set.add(n);
-                sum += n;
             }
         }
-        int missing = legalsum - sum;
+        int missing = legalsum + dupli;
         return new int[]{dupli, missing};
     }
 }
