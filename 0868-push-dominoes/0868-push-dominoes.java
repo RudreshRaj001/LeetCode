d4d82a562601248ef493243526ea7353030a5fbf
Rudresh Raj <120813698+RudreshRaj001@users.noreply.github.com>
2025-05-03 21:25:15 +0530

    Time: 23 ms (47.17%), Space: 53.8 MB (9.06%) - LeetHub

--- 0868-push-dominoes/0868-push-dominoes.java
+++ 0868-push-dominoes/0868-push-dominoes.java
@@ -2,31 +2,31 @@ class Solution {
     public String pushDominoes(String dominoes) {
         char[] domi = dominoes.toCharArray();
         int n = dominoes.length();
-        // Queue<int[]> queue = new LinkedList<>(); 
-        Deque<int[]> queue = new ArrayDeque<>();
+        Queue<int[]> queue = new LinkedList<>(); 
+        // Deque<int[]> queue = new ArrayDeque<>();
         for(int i = 0; i < n ; i++){
             if(domi[i] != '.'){
                 if(domi[i] == 'R'){
-                    queue.offerLast(new int[]{i, 1});
+                    queue.offer(new int[]{i, 1});
                 }else{
-                    queue.offerLast(new int[]{i, 0});
+                    queue.offer(new int[]{i, 0});
                 }
             }
         }
 
         while(!queue.isEmpty()){
-            int[] current = queue.pollFirst();
+            int[] current = queue.poll();
             int i = current[0]; 
             char ch = current[1] == 1 ? 'R' : 'L';
 
             if(ch == 'L' && i > 0 && domi[i - 1] == '.'){
-                queue.offerLast(new int[]{i - 1, 0});
+                queue.offer(new int[]{i - 1, 0});
                 domi[i - 1] = 'L';
             }else if(ch == 'R' && i + 1 < n && domi[i + 1] == '.'){
                 if(i + 2 < n && domi[i + 2] == 'L'){
-                    queue.pollFirst();
+                    queue.poll();
                 }else{
-                    queue.offerLast(new int[]{i + 1, 1});
+                    queue.offer(new int[]{i + 1, 1});
                     domi[i + 1] = 'R';
                 }
             }
