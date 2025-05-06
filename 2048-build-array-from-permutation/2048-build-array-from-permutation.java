586a0034b79f44d9723a44b014e03a685e72d300
Rudresh Raj <120813698+RudreshRaj001@users.noreply.github.com>
2025-05-06 09:42:38 +0530

    Time: 2 ms (7.3%), Space: 45.4 MB (63.62%) - LeetHub

--- 2048-build-array-from-permutation/2048-build-array-from-permutation.java
+++ 2048-build-array-from-permutation/2048-build-array-from-permutation.java
@@ -3,6 +3,10 @@ class Solution {
         int n = nums.length;
         for (int i = 0; i < n; i++) {
             nums[i] += n * (nums[nums[i]] % n);
+            // trying to store 2 values at a single place:
+            // the current value and the futur value
+            // nums[nums[i]] % n -> if gives the current value
+            // nums[i] /= n -> gives the future value
         }
         for (int i = 0; i < n; i++) {
             nums[i] /= n;
